d0efdf01c279d69ee9479e10e1fbc15f7294fb01
lupo996 <lupo996@gmail.com>
2011-11-19 09:43:28 +0000

    With this version of the script I finally added the name check and the Credits.
    I've also indented the code.

--- script_it.sh
+++ script_it.sh
@@ -1,3 +1,11 @@
+#Script_it, a simple script which simply tidy your .cap packets collection and prepare them for cracking with oclHashcat-Plus.
+#Concept by Hash-IT
+#Code by SmilingWolf
+echo "Script_it, a simple script which simply tidy your .cap packets collection and prepare them for cracking with oclHashcat-Plus."
+echo "You're free to redistribute this script anywhere you want, but keep the original credits. Thank You."
+echo "Concept by Hash-IT"
+echo "Code by SmilingWolf"
+sleep 3
 ls -lh A/*
 mkdir -p B/OriginalCaps
 cp A/* B/OriginalCaps/
@@ -6,10 +14,10 @@ mkdir B/UniqueCaps
 md5sum B/OriginalCaps/* > /tmp/hashes_files.tmp
 LIST=`md5sum B/OriginalCaps/* | cut -b -32 | sort | uniq`
 for MD5 in $LIST
-do
-ULIST=`grep $MD5 /tmp/hashes_files.tmp | head -1 | cut -b 35-`
-DLIST=`grep $MD5 /tmp/hashes_files.tmp | head -1 | cut -b 35- | cut -b 16-`
-cp "$ULIST" "B/UniqueCaps/$DLIST"
+  do
+  ULIST=`grep $MD5 /tmp/hashes_files.tmp | head -1 | cut -b 35-`
+  DLIST=`grep $MD5 /tmp/hashes_files.tmp | head -1 | cut -b 35- | cut -b 16-`
+  cp "$ULIST" "B/UniqueCaps/$DLIST"
 done
 echo "Done."
 echo "Remaining packets:"
@@ -17,16 +25,26 @@ ls -lh B/UniqueCaps/*
 mkdir B/CleanCaps
 c=1
 for f in B/UniqueCaps/*
-do
-l=`wpaclean /dev/null "$f" | tail -2 | head -1 | cut -b 23-`
-wpaclean "B/CleanCaps/$l ($c) clean.cap" "$f"
-c=$(( $c + 1 ))
+  do
+  l=`wpaclean /dev/null "$f" | tail -2 | head -1 | cut -b 23-`
+  if [ -e "B/CleanCaps/$l clean.cap" ];
+    then
+    if [ -e "B/CleanCaps/$l ($c) clean.cap" ];
+      then
+      c=$(( $c + 1 ))
+      else
+      wpaclean "B/CleanCaps/$l ($c) clean.cap" "$f"
+    fi
+    else
+    wpaclean "B/CleanCaps/$l clean.cap" "$f"
+    c=1
+  fi
 done
 ls -lh B/CleanCaps/*
 mkdir B/HCcaps
 for f in B/CleanCaps/*
-do
-n=`echo -n "$f" | cut -b 13- | sed s/\ clean\.cap//g`
-aircrack-ng -J "B/HCcaps/$n" "$f"
+  do
+  n=`echo -n "$f" | cut -b 13- | sed s/\ clean\.cap//g`
+  aircrack-ng -J "B/HCcaps/$n" "$f"
 done
 ls -lh B/HCcaps/*
