63634a2c73f79d7605a37ca909f932145be0bc94
lupo996 <lupo996@gmail.com>
2011-11-19 09:40:43 +0000

    File comparison added.
    While it still adds the number between the ESSID and the "clean" word, now checks for duplicates and eventually removes them before passing them through wpaclean.
    The unique caps are stored in the B/UniqueCaps folder.
    
    The comparison is made using the md5sum program (included in most Linux distribution).

--- script_it.sh
+++ script_it.sh
@@ -1,9 +1,22 @@
-ls -lh A/
+ls -lh A/*
 mkdir -p B/OriginalCaps
 cp A/* B/OriginalCaps/
+echo "Deleting duplicated packets..."
+mkdir B/UniqueCaps
+md5sum B/OriginalCaps/* > /tmp/hashes_files.tmp
+LIST=`md5sum B/OriginalCaps/* | cut -b -32 | sort | uniq`
+for MD5 in $LIST
+do
+ULIST=`grep $MD5 /tmp/hashes_files.tmp | head -1 | cut -b 35-`
+DLIST=`grep $MD5 /tmp/hashes_files.tmp | head -1 | cut -b 35- | cut -b 16-`
+cp "$ULIST" "B/UniqueCaps/$DLIST"
+done
+echo "Done."
+echo "Remaining packets:"
+ls -lh B/UniqueCaps/*
 mkdir B/CleanCaps
 c=1
-for f in A/*
+for f in B/UniqueCaps/*
 do
 l=`wpaclean /dev/null "$f" | tail -2 | head -1 | cut -b 23-`
 wpaclean "B/CleanCaps/$l ($c) clean.cap" "$f"
